79f12dd97e2bd778450fb7f385f8807a880330c9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-10-30 22:04:14 -0300

    initial commit

---
+++ src/main/java/br/com/pedromoreira/ecopoint/EcopointApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedromoreira.ecopoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcopointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedromoreira/ecopoint/EcopointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedromoreira.ecopoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcopointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
