daab6cde878d250c52c0e7d9de85ccbd246fafce
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-10-31 14:43:17 -0300

    feat: adicionado repository para location_item

---
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location_item/Location_ItemRepository.java
@@ -0,0 +1,6 @@
+package br.com.pedromoreira.ecopoint.api.location_item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Location_ItemRepository extends JpaRepository<Location_Item, Long> {
+}
