b398a85ab1042f7b1b98c1f440e97fd8efb1148a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-10-31 14:54:45 -0300

    feat: adicionado insert para itemService

--- src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemService.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemService.java
@@ -2,6 +2,7 @@ package br.com.pedromoreira.ecopoint.api.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +21,10 @@ public class ItemService {
     public ItemDTO getItemById(Long id){
         Optional<Item> item = rep.findById(id);
         return item.map(ItemDTO::create).orElse(null);
+    }
+
+    public ItemDTO insert(Item item){
+        Assert.isNull(item.getId(), "Não foi possível inserir o registro.");
+        return ItemDTO.create(rep.save(item));
     }
 }
