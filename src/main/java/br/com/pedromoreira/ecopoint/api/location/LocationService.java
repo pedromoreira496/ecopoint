c69d6b402b9b8ebae55cab08ba42d39d564d859f
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-10-31 15:11:52 -0300

    feat: adicionado getLocations para locationService

--- src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationService.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationService.java
@@ -3,8 +3,15 @@ package br.com.pedromoreira.ecopoint.api.location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class LocationService {
     @Autowired
     private LocationRepository rep;
+
+    public List<LocationDTO> getLocations(){
+        return rep.findAll().stream().map(LocationDTO::create).collect(Collectors.toList());
+    }
 }
