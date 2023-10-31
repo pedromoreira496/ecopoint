9e0fec92cd6d9e26b3434e914a26202a6a528411
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-10-31 14:50:43 -0300

    feat: adicionado getItems para itemService

--- src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemService.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemService.java
@@ -3,9 +3,16 @@ package br.com.pedromoreira.ecopoint.api.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ItemService {
 
     @Autowired
     private ItemRepository rep;
+
+    public List<ItemDTO> getItems() {
+        return rep.findAll().stream().map(ItemDTO::create).collect(Collectors.toList());
+    }
 }
