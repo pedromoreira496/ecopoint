afc7df465dc080e15ae1a5b91fffc0651fb91cf2
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-01 07:38:37 -0300

    feat: adicionado service para location_item

---
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location_item/Location_ItemService.java
@@ -0,0 +1,11 @@
+package br.com.pedromoreira.ecopoint.api.location_item;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Location_ItemService {
+
+    @Autowired
+    private Location_ItemRepository rep;
+}
