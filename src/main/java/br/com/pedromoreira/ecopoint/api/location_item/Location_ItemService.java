3c85510d7ba4cd0bf53dcb1a5a6ae6feb16492ca
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-01 07:40:49 -0300

    feat: adicionado getLocation_Items para location_item

--- src/main/java/br/com/pedromoreira/ecopoint/api/location_item/Location_ItemService.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location_item/Location_ItemService.java
@@ -3,9 +3,16 @@ package br.com.pedromoreira.ecopoint.api.location_item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class Location_ItemService {
 
     @Autowired
     private Location_ItemRepository rep;
+
+    public List<Location_ItemDTO> getLocation_Items() {
+        return rep.findAll().stream().map(Location_ItemDTO::create).collect(Collectors.toList());
+    }
 }
