77711e4c58e0b92210facd46501b79f7cf91ab04
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-01 07:55:25 -0300

    feat: adicionado getUserById para user

--- src/main/java/br/com/pedromoreira/ecopoint/api/user/UserService.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/user/UserService.java
@@ -2,8 +2,10 @@ package br.com.pedromoreira.ecopoint.api.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<UserDTO> getUsers() {
         return rep.findAll().stream().map(UserDTO::create).collect(Collectors.toList());
+    }
+
+    public UserDTO getUserById(Long id){
+        Optional<User> user = rep.findById(id);
+        return user.map(UserDTO::create).orElse(null);
     }
 }
