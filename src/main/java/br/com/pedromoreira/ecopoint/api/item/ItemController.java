e6a856d924b1c1a1bbb24929003259e5e41686c5
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-02 20:15:30 -0300

    feat: adicionado funcao selectAll para ItemController

--- src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemController.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemController.java
@@ -1,8 +1,12 @@
 package br.com.pedromoreira.ecopoint.api.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/items")
@@ -10,4 +14,9 @@ public class ItemController {
 
     @Autowired
     private ItemService service;
+
+    @GetMapping
+    public ResponseEntity<List<ItemDTO>> selectAll(){
+        return ResponseEntity.ok(service.getItems());
+    }
 }
