5a28f04edcdef11084387975a39400406988969c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-02 20:33:01 -0300

    feat: adicionado funcao selectById para ItemController

--- src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemController.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/item/ItemController.java
@@ -3,6 +3,7 @@ package br.com.pedromoreira.ecopoint.api.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,11 @@ public class ItemController {
     @GetMapping
     public ResponseEntity<List<ItemDTO>> selectAll(){
         return ResponseEntity.ok(service.getItems());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ItemDTO> selectById(@PathVariable("id") Long id){
+        ItemDTO item = service.getItemById(id);
+        return item != null ? ResponseEntity.ok(item) : ResponseEntity.notFound().build();
     }
 }
