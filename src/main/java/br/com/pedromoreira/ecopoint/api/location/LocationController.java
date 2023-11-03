f0473e089534a1e78aa76fb1b1fcebb7f192a7ee
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-03 16:46:06 -0300

    feat: adicionado selectById para location controller

--- src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationController.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationController.java
@@ -3,6 +3,7 @@ package br.com.pedromoreira.ecopoint.api.location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class LocationController {
     @GetMapping
     public ResponseEntity<List<LocationDTO>> selectAll(){
         return ResponseEntity.ok(service.getLocations());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<LocationDTO> selectById(@PathVariable("id") Long id){
+        LocationDTO location = service.getLocationById(id);
+        return location != null ? ResponseEntity.ok(location) : ResponseEntity.notFound().build();
     }
 
 }
