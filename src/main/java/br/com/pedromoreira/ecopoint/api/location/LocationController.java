0b8511d464862709454c37e2481e8e678222d91c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-03 16:43:09 -0300

    feat: adicionado selectAll para location controller

--- src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationController.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/location/LocationController.java
@@ -1,8 +1,12 @@
 package br.com.pedromoreira.ecopoint.api.location;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -11,5 +15,10 @@ public class LocationController {
 
     @Autowired
     private LocationService service;
+
+    @GetMapping
+    public ResponseEntity<List<LocationDTO>> selectAll(){
+        return ResponseEntity.ok(service.getLocations());
+    }
 
 }
