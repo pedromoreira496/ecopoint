861b99b04fd15831c4132d465f1cc968a5b18c06
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-03 16:25:56 -0300

    feat: adicionado propriedades para security em user

--- src/main/java/br/com/pedromoreira/ecopoint/api/user/User.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/user/User.java
@@ -3,17 +3,22 @@ package br.com.pedromoreira.ecopoint.api.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -21,4 +26,43 @@ public class User {
     private String nome;
     private String email;
     private String password;
+    private UserRole role;
+
+    public User(String nome, String email, String password, UserRole role){
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
