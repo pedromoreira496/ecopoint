7ffe41142bd2e6821ad182c129be8496ed470f48
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-06 05:56:31 -0300

    feat: adicionado arquivo sql para popular banco de testes

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (id, titulo, imagem) VALUES (1, 'Plastico', 'plastico.png');
+INSERT INTO `items` (id, titulo, imagem) VALUES (2, 'Papelao', 'papelao.png');
+INSERT INTO `items` (id, titulo, imagem) VALUES (3, 'Pilha', 'pilha.png');
