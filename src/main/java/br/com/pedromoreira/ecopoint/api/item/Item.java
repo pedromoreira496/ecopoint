98cb33e38b967a93dc6415ac36d898e5511dc0fa
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-06 04:03:37 -0300

    refac: adicionado validation para item

--- src/main/java/br/com/pedromoreira/ecopoint/api/item/Item.java
+++ src/main/java/br/com/pedromoreira/ecopoint/api/item/Item.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import javax.persistence.*;
 import java.util.Set;
 
@@ -17,7 +18,11 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+    @NotBlank(message = "O titulo nao pode ser vazio")
+    @Size(min = 5, max = 40, message = "O titulo precisa ter entre 10 a 40 caracteres")
     private String titulo;
+
+    @NotBlank(message = "É necessário haver o envio de uma imagem")
     private String imagem;
 
     @OneToMany(mappedBy = "item")
