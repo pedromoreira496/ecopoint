693fbb9864d557e5d2c37725fdf6248f6f0fcb3b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-11-06 21:55:47 -0300

    refac: adicionado user para testes

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO `items` (id, titulo, imagem) VALUES (1, 'Plastico', 'plastico.png');
 INSERT INTO `items` (id, titulo, imagem) VALUES (2, 'Papelao', 'papelao.png');
 INSERT INTO `items` (id, titulo, imagem) VALUES (3, 'Pilha', 'pilha.png');
+
+INSERT INTO `users` (id, email, nome, password) VALUES (1, 'pedro@moreira.com', 'Pedro moreira', '{HASH_PASSWORD}');
